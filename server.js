8510b840e47bd463684c2297e96112c2c6741d5b
Simone Taeggi <startae14@gmail.com>
2019-03-11 00:02:11 +0100

    remove locks

--- server.js
+++ server.js
@@ -26,7 +26,15 @@ app.post('/api/convert', (req, res) => {
         console.log(error.message);
         res.status(500).json({msg: error.message});
     }
+});
 
-  });
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
